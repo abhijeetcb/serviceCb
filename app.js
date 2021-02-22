4aa89400781a10c1ed5d9ce47ba49058cf505bfe
abhijeetcb <65212150+abhijeetcb@users.noreply.github.com>
2021-02-22 16:11:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
 });
 
 const server = app.listen(3008, () => {
-  logger.info(`Listening to port ${process.env.PORT}`);
+  logger.info(`Listening to port 3008`);
 });
 server.timeout = 600000; // 10 minutes
 
