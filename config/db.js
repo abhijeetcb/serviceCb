4a727d0a717d680e9c40ffa7e4f8c2a208559107
abhijeetcb <65212150+abhijeetcb@users.noreply.github.com>
2021-02-22 15:31:30 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_HOST, { logging: false });
+const sequelize = new Sequelize("postgres://janani:janani2021@report.cmfnjrzpxm2b.ap-south-1.rds.amazonaws.com/report", { logging: false });
 
 sequelize.authenticate().then(() => {
   console.log('Success!');
